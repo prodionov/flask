1b1c30e141da6eb51402868727222372d5559724
prodionov <pavelrodionov@hotmail.com>
2018-05-30 13:30:52 +0100

    form and user input

--- textAPI.py
+++ textAPI.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -22,20 +22,29 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template(['index.html', 'navigation.html'], posts=posts)
+    return render_template('index.html', posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('home'))
     return render_template("register.html", title="Register", form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash(f'You have been logged in!', 'success')
+            return redirect (url_for('home'))
+        else:
+            flash('Login Unsuccessful, Please check username and password', 'danger')
     return render_template("login.html", title="Login", form=form)
 
 
